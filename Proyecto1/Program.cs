b5f2c6bfbae60feeba0c751b9083d145585dec04
teebif <tebyforloni@gmail.com>
2023-02-06 20:30:08 -0300

    Actualizacion
    Modelos de objetos + Clase manejador

--- Proyecto1/Program.cs
+++ Proyecto1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿
+using System.Data.SqlClient;
 
 public class Program
 {
@@ -6,5 +7,20 @@ public class Program
     {
         string connectionstring = "Data Source=DESKTOP-HPHJBO6;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         SqlConnection connection = new SqlConnection(connectionstring);
+        
+
+        SqlCommand comand = new SqlCommand("Select * from Usuario", connection);
+        connection.Open();
+        SqlDataReader reader = comand.ExecuteReader();
+        if (reader.HasRows)
+        {
+            while(reader.Read())
+            {
+                Console.WriteLine(reader.GetInt64(0)); 
+            }
+        }
+
+        connection.Close();
+
     }
 }
