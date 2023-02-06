4952df6f7216d0db2a7f9b3b6b018ac3e6c69ef7
teebif <tebyforloni@gmail.com>
2023-02-06 18:46:29 -0300

    Actualizacion modelos
    4 modelos de los objetos utilizados en la base de datos

--- Proyecto1/Program.cs
+++ Proyecto1/Program.cs
@@ -1,9 +1,10 @@
-﻿using Proyecto1.Clases;
+﻿using System.Data.SqlClient;
 
-class Programa
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("hola");
+        string connectionstring = "Data Source=DESKTOP-HPHJBO6;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionstring);
     }
 }
