3ddc84b0e4a1bcef8d4356eb265c90cd8869341b
teebif <tebyforloni@gmail.com>
2023-02-06 23:54:27 -0300

    ordenar

--- Proyecto1/Manejador.cs
+++ Proyecto1/Manejador.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Proyecto1
 {
-    internal class Class1
+    public class Manejador
     {
+        public static string  connectionstring = "Data Source=DESKTOP-HPHJBO6;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionstring);
+        
+
+        SqlCommand comand = new SqlCommand("Select * from Usuario", connection);
+        connection.Open();
+        SqlDataReader reader = comand.ExecuteReader();
+            if (reader.HasRows)
+                 {
+                 while(reader.Read())
+                {
+                Console.WriteLine(reader.GetInt64(0)); 
+                 }
+            
+
+            connection.Close();
+        }
     }
 }
--- Proyecto1/Program.cs
+++ Proyecto1/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using System.Data.SqlClient;
-
+﻿using System.Data.SqlClient;
 public class Program
 {
     static void Main(string[] args)
@@ -8,7 +6,6 @@ public class Program
         string connectionstring = "Data Source=DESKTOP-HPHJBO6;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         SqlConnection connection = new SqlConnection(connectionstring);
         
-
         SqlCommand comand = new SqlCommand("Select * from Usuario", connection);
         connection.Open();
         SqlDataReader reader = comand.ExecuteReader();
